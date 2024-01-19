92f0974517604be1618838b2eee2125584eebddf
Rakshya Khanal <rakshyakhanal412@gmail.com>
2024-01-19 17:59:17 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Navbar from './Components/Navbar';
+import Intro from './Components/Intro';
+import Feature from './Components/Feature';
+import Service from './Components/Service';
+import Price from './Components/Price';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+
+const Container = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  `;
+
+const Shape = css`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  `;
+
+const IntoShape = styled.div`
+  ${Shape}
+  clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+  background-color: crimson; 
+  `;
+
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
+  background-color: pink; 
+  `;
+
+const ServiceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 33% 0%, 33% 100%, 0 100%);
+  background-color: #f88497; 
+  `;
+
+const PriceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
+  background-color: crimson; 
+  `;
+
+
+const App = () => {
+  const smallScreen = window.screen.width <= 480 ? true : false;
+  return (
+    <>
+      <Container>
+        <Navbar />
+        <Intro />
+        <IntoShape />
+      </Container>
+
+      <Container>
+        <Feature />
+        <FeatureShape />
+      </Container>
+
+      <Container>
+        <Service />
+        {!smallScreen && <ServiceShape />}
+      </Container>
+
+      <Container>
+        <Price />
+        <PriceShape />
+      </Container>
+
+      <Container>
+        <Contact />
+        <Footer />
+      </Container>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Price.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import PriceCard from './PriceCard';
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+  }
+`;
+
+const Price = () => {   
+  return (
+    <Container>
+        <PriceCard />
+        <PriceCard />
+        <PriceCard />
+    </Container>
+  )
+}
+
+export default Price;
